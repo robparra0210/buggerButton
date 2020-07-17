d8e2c794b47afd1681c448d68cf656735a1ee4b7
rob0210 <robertoparra0210@gmail.com>
2020-07-17 14:57:36 -0400

    ADD: Cambios producto del reloj

--- ham.js
+++ ham.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-export default function hamburgerMenu(panelBtn, panel, menuLink){
-	const d = document;
-=======
+
 export default function menu(burgerbutton, menu, menuMain){
 	//burgerbutton.classList.toggle('active')
 	const d = document;
@@ -10,28 +7,4 @@ export default function menu(burgerbutton, menu, menuMain){
 		if( e.target.matches(burgerbutton)|| e.target.matches(`${burgerbutton} *`)){
 			d.querySelector(menu).classList.toggle("menu-active");
 		}
->>>>>>> myVersion
-
-		if( e.target.matches(`${menuMain} *`)){
-			d.querySelector(menu).classList.toggle("menu-active");
-		}
-
-	d.addEventListener("click", e=>{
-		if( e.target.matches(panelBtn) || e.target.matches(`${panelBtn} *`)){
-			d.querySelector(panel).classList.toggle("is-active");
-			d.querySelector(panelBtn).classList.toggle("is-active");
-		}
-
-<<<<<<< HEAD
-
-		if(e.target.matches(menuLink)){
-			d.querySelector(panel).classList.remove("is-active");
-			d.querySelector(panelBtn).classList.remove("is-active");	
-		}
-	})
-	}
-=======
-	}
-)}
-
->>>>>>> myVersion
+})}
