73a5e97e59344a6d5e26bb96f25f5737cc67d1ad
rob0210 <robertoparra0210@gmail.com>
2020-07-17 14:25:29 -0400

    ✅ADD: Complete module 'reloj.js'
    vesion mia del modulo reloj.js esta completa y operativa

--- reloj.js
+++ reloj.js
@@ -1,28 +1,45 @@
-export default function reloj(hora, min,seg){
-    
-    setInterval(horaActual , 1000)
+export default function reloj(hora, min, seg, displayReloj, iniciarReloj, detenerReloj, iniciarAlarma, detenerAlarma) {
 
+
+    document.addEventListener('click', (e) => {
+        
+        setInterval(function horaActual() {
+            const d = document;
+            let fecha = new Date(),
+                hora = fecha.getHours(),
+                minutos = fecha.getMinutes(),
+                segundos = fecha.getSeconds(),
+                $hora = d.querySelector('.hora'),
+                $min = d.querySelector('.min'),
+                $seg = d.querySelector('.seg');
+            
+            	$hora.innerText = hora;
+            	$min.innerText = minutos;
+            	$seg.innerText = segundos; }, 1000);
+
+
+        if (e.target.matches(iniciarReloj)) {
+            document.querySelector(displayReloj).style.display = 'flex';
+            document.querySelector(iniciarReloj).disabled = true;
+            document.querySelector(detenerReloj).disabled = false;
+        }
+
+        if (e.target.matches(detenerReloj)) {
+            document.querySelector(displayReloj).style.display = 'none';
+            document.querySelector(iniciarReloj).disabled = false;
+            document.querySelector(detenerReloj).disabled = true;
+        }
+
+        if (e.target.matches(iniciarAlarma)) {
+            document.getElementById('alarma').play();
+            document.querySelector(iniciarAlarma).disabled = true;
+            document.querySelector(detenerAlarma).disabled = false;
+        }
+
+        if (e.target.matches(detenerAlarma)) {
+            document.getElementById('alarma').load();
+            document.querySelector(detenerAlarma).disabled = true;
+            document.querySelector(iniciarAlarma).disabled = false;
+        }
+    })
 }
-	
-
-function horaActual(){
-	const d=document;
-
-	let fecha = new Date(),
-		hora = fecha.getHours(),
-        minutos = fecha.getMinutes(),
-        segundos = fecha.getSeconds();
-       
-
-    let   $hora = d.querySelector('.hora'),
-      	  $min = d.querySelector('.min'),
-    	  $seg = d.querySelector('.seg');
-
-		  $hora.innerText = hora;
-		  $min.innerText = minutos;
-		  $seg.innerText = segundos;
-    	 
-
-}
-
-
