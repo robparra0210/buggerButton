543f69a40b9926569a9ae9c302a7f8b43eed16e6
Roberto Parra <robertoparra0210@gmail.com>
2020-07-20 18:30:49 -0400

    REF: close set interval

--- reloj.js
+++ reloj.js
@@ -1,30 +1,31 @@
 export default function reloj(hora, min, seg, displayReloj, iniciarReloj, detenerReloj, iniciarAlarma, detenerAlarma) {
 
-
+    let tempo;    
     document.addEventListener('click', (e) => {
         
-        setInterval(function horaActual() {
-            const d = document;
-            let fecha = new Date(),
-                hora = fecha.getHours(),
-                minutos = fecha.getMinutes(),
-                segundos = fecha.getSeconds(),
-                $hora = d.querySelector('.hora'),
-                $min = d.querySelector('.min'),
-                $seg = d.querySelector('.seg');
-            
-            	$hora.innerText = hora;
-            	$min.innerText = minutos;
-            	$seg.innerText = segundos; }, 1000);
-
-
+        
+        
         if (e.target.matches(iniciarReloj)) {
+            tempo= setInterval(function horaActual() {
+                const d = document;
+                let fecha = new Date(),
+                    hora = fecha.getHours(),
+                    minutos = fecha.getMinutes(),
+                    segundos = fecha.getSeconds(),
+                    $hora = d.querySelector('.hora'),
+                    $min = d.querySelector('.min'),
+                    $seg = d.querySelector('.seg');
+                
+                    $hora.innerText = hora;
+                    $min.innerText = minutos;
+                    $seg.innerText = segundos; }, 1000);
             document.querySelector(displayReloj).style.display = 'flex';
             document.querySelector(iniciarReloj).disabled = true;
             document.querySelector(detenerReloj).disabled = false;
         }
 
         if (e.target.matches(detenerReloj)) {
+            clearInterval(tempo)
             document.querySelector(displayReloj).style.display = 'none';
             document.querySelector(iniciarReloj).disabled = false;
             document.querySelector(detenerReloj).disabled = true;
