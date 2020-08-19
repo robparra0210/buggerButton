0fe880a41c2e7cf9941ec11dfe01202aee4279a5
Roberto Parra <robertoparra0210@gmail.com>
2020-08-19 15:51:16 -0400

    ADD:Narrador module

---
+++ carrusel.js
@@ -0,0 +1,53 @@
+const d = document; 
+
+export default function slider(){
+     const $nextBtn = d.querySelector('.slider-btn .next'),
+        $prevBtn=d.querySelector('.slider-btn .prev'),
+        $slides = d.querySelectorAll('.slider-slide');
+
+        let i=0;
+        d.addEventListener('click', (e)=>{
+           
+            if(e.target===$prevBtn){
+                e.preventDefault();
+                $slides[i].classList.remove('active')
+                i--; 
+
+                if(i<0){
+                    i=$slides.length-1;
+                }
+                $slides[i].classList.add('active')
+                clearInterval(x)
+
+            } 
+            
+            if(e.target===$nextBtn){
+                e.preventDefault();
+                $slides[i].classList.remove('active')
+                i++; 
+
+                if(i>=$slides.length){
+                    i=0;
+                }
+                $slides[i].classList.add('active')
+                clearInterval(x)
+
+            }
+        })
+
+
+        const x= setInterval((e) => {
+            $slides[i].classList.remove('active')
+            i++; 
+            if(i>=$slides.length){
+                i=0;
+            }
+            $slides[i].classList.add('active')
+            }, 5000);
+
+        
+
+
+
+
+}
